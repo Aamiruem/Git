28a9b52272c1946bd97afded42ad793658be4cfa
Aamir@786 <aamirhussainazad786@gmail.com>
2024-07-28 23:53:28 +0530

    commit add

---
+++ four/GitTests.java
@@ -0,0 +1,10 @@
+package Git.four;
+
+public class GitTests {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("This is a test");
+        System.out.println("This is a new feature");
+    }
+}
